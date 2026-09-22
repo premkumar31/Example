bc02412caa6417282ec5f94aa73a1d031cf27e18
sku <sukesh.mk@lucastvs.co.in>
2026-09-22 15:04:28 +0530

    Hello World Program added as example

---
+++ build.bat
@@ -0,0 +1,5 @@
+@echo off
+set gcc=gcc
+set out=Hello.exe
+
+gcc main.c -o %out%
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // This function prints text to the screen
+    printf("Hello, World!\n");
+    
+    return 0;
+}
